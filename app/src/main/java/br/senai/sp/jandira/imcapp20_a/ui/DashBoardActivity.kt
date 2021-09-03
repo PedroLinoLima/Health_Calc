ad9b2a0a8168c9ae3299c295ee5e86415c498e02
pedrolinodelima@outlook.com <20272589@cfp127aluno.local>
2021-09-03 09:50:06 -0300

    Projeto-Healt_Calc

---
+++ app/src/main/java/br/senai/sp/jandira/imcapp20_a/ui/DashBoardActivity.kt
@@ -0,0 +1,50 @@
+package br.senai.sp.jandira.imcapp20_a.ui
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.senai.sp.jandira.imcapp20_a.R
+import br.senai.sp.jandira.imcapp20_a.utils.converterBase64ParaBitmap
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_dash_board.*
+
+class DashBoardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dash_board)
+
+        preencherDashBoard()
+
+        tv_logout.setOnClickListener {
+            val dados = getSharedPreferences("dados_usuario", Context.MODE_PRIVATE)
+            val editor = dados.edit()
+            editor.putBoolean("lembrar", false)
+            editor.apply()
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+    private fun preencherDashBoard() {
+        val dados = getSharedPreferences("dados_usuario", Context.MODE_PRIVATE)
+
+        tv_profile_name.text = dados.getString("nome", "")
+        tv_profile_occupation.text = dados.getString("profissao", "")
+        tv_weight.text = dados.getInt("peso", 0).toString()
+        tv_age.text = dados.getString("idade", "")
+
+        val imagemBase64 = dados.getString("foto", "")
+        val imagemBitmap = converterBase64ParaBitmap(imagemBase64)
+
+        iv_profile.setImageBitmap(imagemBitmap)
+
+        // *** Colocar foto do Github no ImageView
+//        val url = "https://avatars.githubusercontent.com/u/14265058?v=4"
+//        Glide.with(this).load(url).into(iv_profile)
+
+    }
+}
